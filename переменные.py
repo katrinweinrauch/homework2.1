06744b7aee7daffa05ca6f28449b804f5ce6fb47
katrinweinrauch <katrinweinrauch1@yandex.ru>
2024-07-29 21:04:39 +0500

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,7 @@
+amount_of_homework = '12'
+number_of_hours = '1.5'
+name_of_course = 'Python'
+time_for_one_task = str((float(number_of_hours) / float(amount_of_homework)))
+print('Курс: ' + name_of_course + ',' + ' всего задач: ' + amount_of_homework + ',' + ' затрачено часов: ' + number_of_hours + ',' + ' среднее время выполнения: ' + time_for_one_task + '.')
+
+print((float(number_of_hours) / float(amount_of_homework)))
